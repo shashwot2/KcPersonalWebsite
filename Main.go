1a7b53b206b719369417eb74ab22d22b26b89b40
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2022-04-02 07:57:19 +0000

    Added proper format and placeholder page

--- Main.go
+++ Main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+  //"fmt"
+  "html/template"
+  "net/http"
+  "log"
+)
+var tpl *template.Template
+func init() {
+  tpl = template.Must(template.ParseGlob("public/templates/*.gohtml"))
+}
 func main() {
+  http.HandleFunc("/", indexHandler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+  
+}
 
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+  err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+  if err != nil {
+    log.Println(err)
+    http.Error(w, "Internal server error", http.StatusInternalServerError)
+    return
+  }
 }
